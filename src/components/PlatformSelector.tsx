17c0963e137e2e503a64177c7385b7807e07cd83
ali591195 <ali.zahid3428@gmail.com>
2023-09-04 17:00:12 +0500

    Refactor -simplify game query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,26 +4,25 @@ import usePlatforms, { Platform } from "../hooks/usePlatform";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
+  selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatforms();
+
+  const platform = data.results.find((p) => p.id === selectedPlatformId);
 
   if (error) return null;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || "Platforms"}
+        {platform?.name || "Platform"}
       </MenuButton>
       <MenuList>
-        {data?.results.map((platform) => (
-          <MenuItem
-            onClick={() => onSelectPlatform(platform)}
-            key={platform.id}
-          >
-            {platform.name}
+        {data?.results.map((p) => (
+          <MenuItem onClick={() => onSelectPlatform(p)} key={p.id}>
+            {p.name}
           </MenuItem>
         ))}
       </MenuList>
